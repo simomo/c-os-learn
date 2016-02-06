769864208f7f7b3a9dc5fec3825187edff6cb26d
duozhang <duozhang@vmware.com>
2016-02-06 11:55:25 -0800

    working on linked_list

--- linux_kernel/data_structure/linked_list.c
+++ linux_kernel/data_structure/linked_list.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
 #include <stddef.h>
+
+#define container_of(ptr, type, member) ({                      \
+        const typeof( ((type *)0)->member ) *__mptr = (ptr);    \
+        (type *)( (char *)__mptr - offsetof(type,member) );})
+
+
+struct list_head {
+    struct list_head* prev;
+    struct list_head* next;
+};
+
+struct User {
+    int id;
+    char* name;
+
+    struct list_head* list;
+};
+
+void print_user (struct User * p_u) {
+    printf("id %d, name %s\n", p_u->id, p_u->name);
+}
+
+int main () {
+    struct User user = {.id = 1, .name = "test"};
+    struct User* p_u = &user;
+    print_user(&user);
+
+    return 1;
+}
