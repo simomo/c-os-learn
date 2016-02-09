b3d2bdddaee105eec17fa524a94f9a2c0b5cf290
duozhang <duozhang@vmware.com>
2016-02-09 01:53:00 -0800

    working on list ops

--- linux_kernel/data_structure/linked_list.c
+++ linux_kernel/data_structure/linked_list.c
@@ -6,10 +6,40 @@
         (type *)( (char *)__mptr - offsetof(type,member) );})
 
 
+
+//TODO: iterator whole list
+#define ITER_LIST(
+
+
 struct list_head {
     struct list_head* prev;
     struct list_head* next;
 };
+
+void inline init_list(list_head* list) {
+    list->prev = list;
+    list->next = list;
+}
+
+//TODO: Insert item at head
+void inline insert_list_head(list_head* list, list_head* item) {
+    item->prev = list;
+    item->next = list->next;
+    list->next = item;
+}
+
+//TODO: Insert item at tail
+void inline insert_list_tail(list_head* list, list_head* item) {
+    item->prev = list->prev;
+    item->next = list;
+    list->prev = item;
+}
+
+//TODO: remove one item
+void inline remove_list(list_head* list, list_head* item) {
+    item->prev->next = item->next;
+    item->next->prev = item->prev;
+}
 
 struct User {
     int id;
