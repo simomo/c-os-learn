3dcf277ba8a819cae51b4c36cb44e6b8dafc7c8b
Duo Zhang <qihangzp@gmail.com>
2016-07-10 14:15:57 +0000

    disassemble binary

--- other_learning/array_name/array_name.c
+++ other_learning/array_name/array_name.c
@@ -3,12 +3,18 @@
 //
 #include <stdio.h>
 
+void my_print(int* a);
 
 int main() {
     int a[3][2] = {{1, 2}, {3, 4}, {5, 6}};
 
-    printf("a: %d\n", (int) **a);
-    printf("a[0]: %d\n", a[0]);
-    printf("a[0][0]: %d\n", a[0][0]);
+    my_print(a);
+
+    //printf("a: %d\n", (int) **a);
+    //printf("a[0]: %d\n", a[0]);
+    //printf("a[0][0]: %d\n", a[0][0]);
 }
 
+void my_print(int* a) {
+    printf("p: %d\n", a);
+}
